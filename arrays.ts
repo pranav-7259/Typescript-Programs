064da5dd324f0c6b544fcb3a7a3b03832dc8e01d
Pranav Shekhar <pranav.shekhar@learner.manipal.edu>
2017-07-19 12:21:44 +0530

    Update arrays.ts

--- arrays.ts
+++ arrays.ts
@@ -3,9 +3,13 @@ var names : string[] = ["Pranav" , "Amit" , "Rubak"] // ';' is optional
 for(let i=0 ; i<names.length ; i++)
     console.log(names[i]);
 
+names.push("Abhishek") // Add one element to the array
+
 //id reads index 
 for(let id in names)
     console.log(names[id]);
+
+names.splice(1,1) // Remove one element from 1st pos
 
 //n reads values
 for(let n of names)
